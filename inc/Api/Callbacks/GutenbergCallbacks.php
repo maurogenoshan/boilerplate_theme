7ebab435945e43ca65e161d12f3192aad26a7873
Mauro <mauro@genosha.com.ar>
2021-09-22 18:00:44 -0300

    Crea Blques de Gutenberg

---
+++ inc/Api/Callbacks/GutenbergCallbacks.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * T
+ *
+ * @package genosha
+ */
+
+namespace App\Api\Callbacks;
+
+
+/**
+ * GutenbergCallbacks class
+ */
+class GutenbergCallbacks
+{
+	/**
+	 * register default hooks and actions for WordPress
+	 * @return
+	 */
+	public function block_default_callback($fields, $attributes, $inner_blocks) 
+	{
+		?>
+
+		<div class="block">
+			<div class="block__heading">
+				<h1><?php echo esc_html( $fields['heading'] ); ?></h1>
+			</div><!-- /.block__heading -->
+
+			<div class="block__image">
+				<?php echo wp_get_attachment_image( $fields['image'], 'full' ); ?>
+			</div><!-- /.block__image -->
+
+			<div class="block__content">
+				<?php echo apply_filters( 'the_content', $fields['content'] ); ?>
+			</div><!-- /.block__content -->
+		</div><!-- /.block -->
+
+		<?php
+	}
+
+	
+}
--- inc/Api/Gutenberg.php
+++ inc/Api/Gutenberg.php
@@ -1,14 +1,17 @@
 <?php
+namespace App\Api;
 /**
  * Build Gutenberg Blocks
  *
  * @package genosha
  */
 
-namespace App\Api;
+use Carbon_Fields\Block;
+use Carbon_Fields\Field;
+use App\Api\Callbacks\GutenbergCallbacks;
 
 /**
- * Customizer class
+ * Gutenberg clase
  */
 class Gutenberg
 {
@@ -17,24 +20,26 @@ class Gutenberg
 	 *
 	 * @return WordPress add_action()
 	 */
+
+	private $styles,$callbacks;
+
 	public function register() 
 	{
-		if ( ! function_exists( 'register_block_type' ) ) {
-			return;
-		}
+		$this->callbacks = new GutenbergCallbacks();
+		add_action( 'init', array( $this, 'gutenberg_config' ) );
+		
+		
+		add_action( 'wp_enqueue_scripts', array($this,'load_styles') );
 
-		add_action( 'init', array( $this, 'gutenberg_init' ) );
+		$this->gutenberg_setup_block();
 
-		add_action( 'init', array( $this, 'gutenberg_enqueue' ) );
-
-		add_action( 'enqueue_block_assets', array( $this, 'gutenberg_assets' ) );
 	}
 
 	/**
 	 * Custom Gutenberg settings
 	 * @return
 	 */
-	public function gutenberg_init()
+	public function gutenberg_config()
 	{
 		add_theme_support( 'gutenberg', array(
 			// Theme supports responsive video embeds
@@ -70,27 +75,55 @@ class Gutenberg
 				'color' => '#b8c2cc',
 			),
 		) );
+
+		
 	}
 
-	/**
-	 * Enqueue scripts and styles of your Gutenberg blocks
-	 * @return
-	 */
-	public function gutenberg_enqueue()
+	private function load_styles()
 	{
-		wp_register_script( 'gutenberg-awps', get_template_directory_uri() . '/assets/dist/js/gutenberg.js', array( 'wp-blocks', 'wp-element', 'wp-editor' ) );
-
-		register_block_type( 'gutenberg-awps/awps-cta', array(
-			'editor_script' => 'gutenberg-awps', // Load script in the editor
-		) );
+		$this->styles = [
+			[
+				'key' => 'crb-my-shiny-gutenberg-block-stylesheet',
+				'url' => get_stylesheet_directory_uri() . '/css/gutenberg/my-shiny-gutenberg-block.css',
+				'type'=> 'front'
+			],
+			[
+				'key' => 'crb-my-shiny-gutenberg-block-stylesheet-admin',
+				'url' => get_stylesheet_directory_uri() . '/css/gutenberg/my-shiny-gutenberg-block.css',
+				'type'=> 'admin'
+			]
+		];
+		$this->register_styles();
 	}
+
+	private function register_styles()
+	{
+		foreach($this->styles as $style){
+			wp_register_style(
+				$style['key'],
+				$style['url']
+			);
+		}
+	}
+
 
 	/**
 	 * Enqueue scripts and styles of your Gutenberg blocks in the editor
 	 * @return
 	 */
-	public function gutenberg_assets()
+	public function gutenberg_setup_block()
 	{
-		wp_enqueue_style( 'gutenberg-awps-cta', get_template_directory_uri() . '/assets/dist/css/gutenberg.css', null );
+		
+		Block::make( __( 'My Shiny Gutenberg Block' ) )
+		->add_fields( array(
+			Field::make( 'text', 'heading', __( 'Block Heading' ) ),
+			Field::make( 'image', 'image', __( 'Block Image' ) ),
+			Field::make( 'rich_text', 'content', __( 'Block Content' ) ),
+		) )->set_render_callback( array($this->callbacks,'block_default_callback') );
+
+
 	}
+
+
 }
+
